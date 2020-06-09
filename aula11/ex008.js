c71866d087dcf5b9350d3f5b48f41e131b1a7d04
Gabriel Bibiano <gabrieljbsp@gmail.com>
2020-06-08 23:05:48 -0300

    aula11

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 61
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) {
+    console.log(`Voce utrapassou o limete de velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
