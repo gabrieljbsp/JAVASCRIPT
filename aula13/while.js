84914de917f8aa2c788cee9605698ad3fccf4601
Gabriel Bibiano <gabrieljbsp@gmail.com>
2020-06-11 09:53:50 -0300

    aula13

---
+++ aula13/while.js
@@ -0,0 +1,19 @@
+//ESTRUTURAS BASICAS DE REPETICAO 'ENQUANTO!'
+
+var c = 1
+do {    
+    console.log(`Linha ${c}`)
+    c++
+} while (c <= 6)
+
+
+
+ /*
+ 
+ var c = 1
+while (c <= 6) {    
+    console.log(`Linha ${c}`)
+    c++
+}
+
+ */
