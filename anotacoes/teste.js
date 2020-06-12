2b263f75d37d5017648ab6684ddc33f6c64815c4
Gabriel Bibiano <gabrieljbsp@gmail.com>
2020-06-11 23:21:54 -0300

    testes

--- anotacoes/teste.js
+++ anotacoes/teste.js
@@ -1,6 +1,32 @@
-var i = 1
-var f = 10
-var c = 20
-for (i; i <= f; i++) {
-    console.log(`${c} X ${i} = ${c * i}`)
-} 
+ 
+//while
+var ini = 0
+var fim = 10
+var pas = 1
+while (ini <= fim){
+    console.log(`${ini} x ${fim} = ${fim * ini}`)
+    ini = ini + pas
+}
+
+
+/* 
+//do while
+var ini = 0
+var fim = 10 
+var pas = 1
+do {
+    console.log(`${ini} x ${fim} = ${fim * ini}`)
+    ini = ini + pas
+}while (ini <= fim)
+*/
+
+
+/* 
+//for
+var ini = 0
+var fim = 10
+var pas = 1
+for (ini; ini <= fim; ini = ini+pas) {
+    console.log(`${ini} x ${fim} = ${fim * ini}`)
+}
+*/
