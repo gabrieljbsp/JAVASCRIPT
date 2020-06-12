472734a5d21b7e1821315305af5f9136eee5304f
Gabriel Bibiano <gabrieljbsp@gmail.com>
2020-06-12 16:04:18 -0300

    AULA17

---
+++ objeto01.js
@@ -0,0 +1,11 @@
+let amigo = {
+    Nome: 'José'
+    , Sexo: 'M'
+    , Peso: 85.4
+    , engordar(p = 0) {
+        console.log('Engordou')
+        this.Peso += p
+    }
+}
+amigo.engordar(2)
+console.log(amigo);
