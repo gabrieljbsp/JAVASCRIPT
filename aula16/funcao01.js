844987165d8f833b731b983ad2315e2566214d3a
Gabriel Bibiano <gabrieljbsp@gmail.com>
2020-06-12 10:58:39 -0300

    aula16

---
+++ aula16/funcao01.js
@@ -0,0 +1,10 @@
+function parinpar(n1) {
+    if (n1 % 2 == 0) {
+        return 'Par!'
+    } else {
+        return 'Impar!'
+    }
+}
+//let res = parinpar(10)
+//console.log(`${res}`)
+console.log(`${parinpar(5)}`)
